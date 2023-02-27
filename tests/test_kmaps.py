3deaad8ae18f2f70e6695a3c1e96ab392187a560
ChangwhaOh <coh4@vols.utk.edu>
2023-02-27 10:47:48 -0500

    init repo

---
+++ tests/test_kmaps.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `kmaps` package."""
+
+
+import unittest
+
+from kmaps import kmaps
+
+
+class TestKmaps(unittest.TestCase):
+    """Tests for `kmaps` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
